cb67574a5e49cd484c38e4062550d492d44793ff
Josadack <josadaqueferreira@outlook.com>
2025-02-25 09:24:18 -0300

    :sparkles: Criação dos componentes: Home - Navbar - Footer

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,37 @@
+
+function Home() {
+    return (
+        <>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vindo!
+                        </h2>
+                        <p className='text-xl'>
+                           Seu bem-estar, a um toque de distância.
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                                >
+                                Cadastrar Produto
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://ik.imagekit.io/oois5ivj4v/Projeto%20bloco%2003/Innovation-bro.svg?updatedAt=1740486101723"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
